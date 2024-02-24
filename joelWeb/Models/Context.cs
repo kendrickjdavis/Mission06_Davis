d325d677eef02a3fed0643e0872f4a8c2272a86f
Kendrick Davis <kdavis01@byu.edu>
2024-02-23 23:25:14 -0700

    final mission07 commit

--- joelWeb/Models/Context.cs
+++ joelWeb/Models/Context.cs
@@ -6,8 +6,21 @@ namespace joelWeb.Models
     {
         public Context(DbContextOptions<Context> options) : base (options) {}
 
-        public DbSet<Application> Applications { get; set; }
-        
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Category> Categories { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder) //This is how I seeded the data
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = 1, CategoryName = "Miscellaneous" },
+                new Category { CategoryId = 2, CategoryName = "Drama" },
+                new Category { CategoryId = 3, CategoryName = "Television" },
+                new Category { CategoryId = 4, CategoryName = "Horror/Suspense" },
+                new Category { CategoryId = 5, CategoryName = "Comedy" },
+                new Category { CategoryId = 6, CategoryName = "Family" },
+                new Category { CategoryId = 7, CategoryName = "Action/Adventure" },
+                new Category { CategoryId = 8, CategoryName = "VHS" }
+            );
+        }
     }
 }
